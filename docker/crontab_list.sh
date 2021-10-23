8d9f67460115ef29c9416f90b057e375da4958a6
zhaozhanzhan <799316652@qq.com>
2021-10-23 08:24:26 +0800

    预售福利机

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -240,3 +240,5 @@
 1 0,11,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 #城城领现金
 0 0-23/1 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
+#预售福利机
+5 0,2 * * * node /scripts/jd_ys.js >> /scripts/logs/jd_ys.log 2>&1
